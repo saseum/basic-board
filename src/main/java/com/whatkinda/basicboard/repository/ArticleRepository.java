d5ca5431f0d9868ce703fd2014cd54bb0d914dce
Jonghwa <dvlpsjh@gmail.com>
2024-02-10 18:30:36 +0900

    querydsl 통한 게시글 및 댓글 검색조건 설정

--- src/main/java/com/whatkinda/basicboard/repository/ArticleRepository.java
+++ src/main/java/com/whatkinda/basicboard/repository/ArticleRepository.java
@@ -1,7 +1,30 @@
 package com.whatkinda.basicboard.repository;
 
+import com.querydsl.core.types.dsl.DateTimeExpression;
+import com.querydsl.core.types.dsl.StringExpression;
 import com.whatkinda.basicboard.domain.Article;
+import com.whatkinda.basicboard.domain.QArticle;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
+import org.springframework.data.querydsl.binding.QuerydslBinderCustomizer;
+import org.springframework.data.querydsl.binding.QuerydslBindings;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
-public interface ArticleRepository extends JpaRepository<Article, Long> {
+@RepositoryRestResource
+public interface ArticleRepository extends
+        JpaRepository<Article, Long>,
+        QuerydslPredicateExecutor<Article>,
+        QuerydslBinderCustomizer<QArticle>
+{
+
+    @Override
+    default void customize(QuerydslBindings bindings, QArticle root) {
+        bindings.excludeUnlistedProperties(true);
+        bindings.including(root.title, root.contents, root.hashtag, root.createdAt, root.createdBy);
+        bindings.bind(root.title).first(StringExpression::containsIgnoreCase);
+        bindings.bind(root.contents).first(StringExpression::containsIgnoreCase);
+        bindings.bind(root.hashtag).first(StringExpression::containsIgnoreCase);
+        bindings.bind(root.createdAt).first(DateTimeExpression::eq);
+        bindings.bind(root.createdBy).first(StringExpression::containsIgnoreCase);
+    }
 }
--- src/main/java/com/whatkinda/basicboard/repository/CommentRepository.java
+++ src/main/java/com/whatkinda/basicboard/repository/CommentRepository.java
@@ -1,7 +1,28 @@
 package com.whatkinda.basicboard.repository;
 
+import com.querydsl.core.types.dsl.DateTimeExpression;
+import com.querydsl.core.types.dsl.StringExpression;
 import com.whatkinda.basicboard.domain.Comment;
+import com.whatkinda.basicboard.domain.QComment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
+import org.springframework.data.querydsl.binding.QuerydslBinderCustomizer;
+import org.springframework.data.querydsl.binding.QuerydslBindings;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
-public interface CommentRepository extends JpaRepository<Comment, Long> {
+@RepositoryRestResource
+public interface CommentRepository extends
+        JpaRepository<Comment, Long>,
+        QuerydslPredicateExecutor<Comment>,
+        QuerydslBinderCustomizer<QComment>
+{
+
+    @Override
+    default void customize(QuerydslBindings bindings, QComment root) {
+        bindings.excludeUnlistedProperties(true);
+        bindings.including(root.contents, root.createdAt, root.createdBy);
+        bindings.bind(root.contents).first(StringExpression::containsIgnoreCase);
+        bindings.bind(root.createdAt).first(DateTimeExpression::eq);
+        bindings.bind(root.createdBy).first(StringExpression::containsIgnoreCase);
+    }
 }
