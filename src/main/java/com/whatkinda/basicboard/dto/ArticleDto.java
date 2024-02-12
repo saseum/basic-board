efedd6f4afb965c11df894704159d8b0ef79ef6b
Jonghwa <dvlpsjh@gmail.com>
2024-02-12 23:36:37 +0900

    ArticleDto 추가

--- src/main/java/com/whatkinda/basicboard/dto/ArticleDto.java
+++ src/main/java/com/whatkinda/basicboard/dto/ArticleDto.java
@@ -1,15 +1,45 @@
 package com.whatkinda.basicboard.dto;
 
+import com.whatkinda.basicboard.domain.Article;
+
 import java.time.LocalDateTime;
 
 public record ArticleDto(
-        LocalDateTime createdAt,
-        String createdBy,
+        Long id,
+        UserAccountDto userAccountDto,
         String title,
         String contents,
-        String hashtag
+        String hashtag,
+        LocalDateTime createdAt,
+        String createdBy,
+        LocalDateTime modifiedAt,
+        String modifiedBy
 ) {
-    public static ArticleDto of(LocalDateTime createdAt, String createdBy, String title, String contents, String hashtag) {
-        return new ArticleDto(createdAt, createdBy, title, contents, hashtag);
+    public static ArticleDto of(Long id, UserAccountDto userAccountDto, String title, String contents, String hashtag,
+                                LocalDateTime createdAt, String createdBy, LocalDateTime modifiedAt, String modifiedBy) {
+        return new ArticleDto(id, userAccountDto, title, contents, hashtag, createdAt, createdBy, modifiedAt, modifiedBy);
+    }
+
+    public static ArticleDto from(Article entity) {
+        return new ArticleDto(
+                entity.getId(),
+                UserAccountDto.from(entity.getUserAccount()),
+                entity.getTitle(),
+                entity.getContents(),
+                entity.getHashtag(),
+                entity.getCreatedAt(),
+                entity.getCreatedBy(),
+                entity.getModifiedAt(),
+                entity.getModifiedBy()
+        );
+    }
+
+    public Article toEntity() {
+        return Article.of(
+                userAccountDto.toEntity(),
+                title,
+                contents,
+                hashtag
+        );
     }
 }
